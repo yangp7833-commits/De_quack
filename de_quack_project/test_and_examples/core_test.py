b1d0f20f0fdc3cc853033e2cecc1593d1858a042
yangp7833-commits <yangp7833@gmail.com>
2026-07-23 21:58:08 +0000

    reinstated manual column mapping and added kwargs as substitute to metadata mapping. Also added a core file test.

---
+++ de_quack_project/test_and_examples/core_test.py
@@ -0,0 +1,31 @@
+from de_quack import de_quackling
+import os
+
+class TestCore():
+    def test_ingestion(self):
+        with de_quackling() as db:
+            db.initialize_gene_table('human')
+            db.initialize_gene_table('mouse')
+            db.ingest('data.txt', metadata = {'experiment_name': 'Test Experiment', 'description': 'This is a test experiment.'}, species = 'human', columns = {'log2FoldChange': 'log2fc'})
+            db.ingest('data1.txt', metadata = {'experiment_name': 'Test Experiment 2', 'description': 'This is another test experiment.'}, species = 'mouse', columns = {'log2FoldChange': 'log2fc'})
+            assert db.get_gene('AKT1').height > 0 and db.get_gene(ensembl_id = 'ENSMUSG00000000017').height > 0
+    
+    def test_queries(self):
+        with de_quackling() as db:
+            assert db.get_downregulated().height == 15
+            assert db.get_upregulated().height == 75
+            assert db.get_experiment(name = 'Test Experiment').height > 0 
+    
+    def test_remove(self):
+        with de_quackling() as db:
+            db.delete_experiment(name = 'Test Experiment')
+            db.delete_experiment(name = 'Test Experiment 2')
+            assert db.get_experiment(name = 'Test Experiment').height == 0
+            assert db.get_experiment(name = 'Test Experiment 2').height == 0
+            
+            
+    def test_cleanup(self):
+        os.remove('SQL.duckdb')
+
+    
+    
--- de_quack_project/test_and_examples/test.py
+++
@@ -1,5 +0,0 @@
-#!/home/codespace/.python/current/bin/python3 
-
-from de_quack import de_quackling, volcano_plot
-    
-    
